a7ce38dd05827049828a689de521d87812f1417d
Jill Vogel <jill.vogel@adelaide.edu.au>
2016-01-25 09:18:33 +1030

    ADX-191 Fixed conflicts in setup script and MANIFEST

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -9,13 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-adelaidex-lti',
-<<<<<<< HEAD
-    version='0.1.3',
-||||||| merged common ancestors
-    version='0.1.2',
-=======
     version='0.3',
->>>>>>> feature-cohort_auth
     packages=['django_adelaidex.lti'],
     include_package_data=True,
     license='Copyright The University of Adelaide, All rights reserved',
@@ -37,16 +31,8 @@ setup(
     install_requires=[
         'Django>=1.7,<1.10',
         'pytz>=2015.2',
-<<<<<<< HEAD
-        'django-adelaidex-util>=0.1',
-        'django-auth-lti==1.2.4',
-||||||| merged common ancestors
-        'django-adelaidex-util>=0.1',
-        'django-auth-lti==1.2.4.1',
-=======
         'django-adelaidex-util>=0.3',
         'django-auth-lti>=1.2.4',
->>>>>>> feature-cohort_auth
     ],
     tests_require=[
         'selenium==2.48.0',
